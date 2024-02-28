03ccff0b38833dfdaee1b76aa415e9c1eec22d15
puneetshettty <143555282+puneetshettty@users.noreply.github.com>
2024-02-28 14:31:51 -0500

    need to make train function

--- Phase2/NeRFModel.py
+++ Phase2/NeRFModel.py
@@ -4,22 +4,141 @@ import numpy as np
 
 
 class NeRFmodel(nn.Module):
-    def __init__(self, embed_pos_L, embed_direction_L):
+    def __init__(
+        self,
+        depth=8,
+        width=256,
+        num_encoding_fn_xyz=6,
+        num_encoding_fn_dir=4,
+    ):
         super(NeRFmodel, self).__init__()
-        #############################
-        # network initialization
-        #############################
 
-    def position_encoding(self, x, L):
-        #############################
-        # Implement position encoding here
-        #############################
+        self.depth = depth
+        self.dim_xyz = 3 + 2 * 3 * num_encoding_fn_xyz
+        self.dim_dir = 3 + 2 * 3 * num_encoding_fn_dir
 
-        return y
+        self.layers_xyz = torch.nn.ModuleList()
+        self.layers_xyz.append(torch.nn.Linear(self.dim_xyz, width))
+        for i in range(1, depth):
+            if i == 4:
+                self.layers_xyz.append(torch.nn.Linear(self.dim_xyz + width, width))
+            else:
+                self.layers_xyz.append(torch.nn.Linear(width, width))
+        self.fc_feat = torch.nn.Linear(width, width)
+        self.fc_alpha = torch.nn.Linear(width, 1)
 
-    def forward(self, pos, direction):
-        #############################
-        # network structure
-        #############################
+        self.layers_dir = torch.nn.ModuleList()
+        self.layers_dir.append(torch.nn.Linear(width + self.dim_dir, 128))
+        for i in range(3):
+            self.layers_dir.append(torch.nn.Linear(128, 128))
+        self.fc_rgb = torch.nn.Linear(128, 3)
+        self.relu = torch.nn.functional.relu
 
-        return output
+    def forward(self, x):
+        xyz, dirs = x[..., : self.dim_xyz], x[..., self.dim_xyz :]
+        for i in range(self.depth):
+            if i == 4:
+                x = self.layers_xyz[i](torch.cat((xyz, x), -1))
+            else:
+                x = self.layers_xyz[i](x)
+            x = self.relu(x)
+        feat = self.fc_feat(x)
+        alpha = self.fc_alpha(feat)
+        x = self.layers_dir[0](torch.cat((feat, dirs), -1))
+        x = self.relu(x)
+        for i in range(1, 3):
+            x = self.layers_dir[i](x)
+            x = self.relu(x)
+        rgb = self.fc_rgb(x)
+        return torch.cat((rgb, alpha), dim=-1)
+
+    def position_encoding(tensor, num_encoding_functions=6, include_input=True, log_sampling=True) -> torch.Tensor:
+        r"""Apply positional encoding to the input.
+
+        Args:
+            tensor (torch.Tensor): Input tensor to be positionally encoded.
+            encoding_size (optional, int): Number of encoding functions used to compute
+                a positional encoding (default: 6).
+            include_input (optional, bool): Whether or not to include the input in the
+                positional encoding (default: True).
+
+        Returns:
+        (torch.Tensor): Positional encoding of the input tensor.
+        """
+        # TESTED
+        # Trivially, the input tensor is added to the positional encoding.
+        encoding = [tensor] if include_input else []
+        frequency_bands = None
+        if log_sampling:
+            frequency_bands = 2.0 ** torch.linspace(
+                0.0,
+                num_encoding_functions - 1,
+                num_encoding_functions,
+                dtype=tensor.dtype,
+                device=tensor.device,
+            )
+        else:
+            frequency_bands = torch.linspace(
+                2.0 ** 0.0,
+                2.0 ** (num_encoding_functions - 1),
+                num_encoding_functions,
+                dtype=tensor.dtype,
+                device=tensor.device,
+            )
+
+        for freq in frequency_bands:
+            for func in [torch.sin, torch.cos]:
+                encoding.append(func(tensor * freq))
+
+        # Special case, for no positional encoding
+        if len(encoding) == 1:
+            return encoding[0]
+        else:
+            return torch.cat(encoding, dim=-1)
+    
+    def position_encoding(self, tensor, num_encoding_functions=6, include_input=True, log_sampling=True) -> torch.Tensor:
+        r"""Apply positional encoding to the input.
+
+        Args:
+            tensor (torch.Tensor): Input tensor to be positionally encoded.
+            encoding_size (optional, int): Number of encoding functions used to compute
+                a positional encoding (default: 6).
+            include_input (optional, bool): Whether or not to include the input in the
+                positional encoding (default: True).
+
+        Returns:
+        (torch.Tensor): Positional encoding of the input tensor.
+        """
+        # Trivially, the input tensor is added to the positional encoding.
+        encoding = [tensor] if include_input else []
+        frequency_bands = None
+        if log_sampling:
+            frequency_bands = 2.0 ** torch.linspace(
+                0.0,
+                num_encoding_functions - 1,
+                num_encoding_functions,
+                dtype=tensor.dtype,
+                device=tensor.device,
+            )
+        else:
+            frequency_bands = torch.linspace(
+                2.0 ** 0.0,
+                2.0 ** (num_encoding_functions - 1),
+                num_encoding_functions,
+                dtype=tensor.dtype,
+                device=tensor.device,
+            )
+
+        for freq in frequency_bands:
+            for func in [torch.sin, torch.cos]:
+                encoding.append(func(tensor * freq))
+
+        # Special case, for no positional encoding
+        if len(encoding) == 1:
+            return encoding[0]
+        else:
+            return torch.cat(encoding, dim=-1)
+
+    def call_position_encoding(self, num_encoding_functions=6, include_input=True, log_sampling=True):
+        r"""Returns a lambda function that internally calls position_encoding."""
+        return lambda x: self.position_encoding(x, num_encoding_functions, include_input, log_sampling)
--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -1,4 +1,3 @@
-import argparse
 import glob
 import json
 import os
@@ -13,6 +12,9 @@ from tqdm import tqdm
 
 from log_setup import logger
 from NeRFModel import *
+
+model = NeRFmodel()
+positional_encoding = model.position_encoding
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 np.random.seed(0)
@@ -53,7 +55,7 @@ def loadDataset(data_path, mode):
     return (width, height, camera_matrix), images, poses
 
     
-def PixelToRay(camera_info, pose, pixelPosition, args):
+def PixelToRay(W, H, focal, pose):
     """
     Input:
         camera_info: image width, height, camera matrix 
@@ -63,40 +65,117 @@ def PixelToRay(camera_info, pose, pixelPosition, args):
     Outputs:
         ray origin and direction
     """
-    pass
+    x = torch.linspace(0, H-1, H)
+    y = torch.linspace(0, W-1, W)
+    x, y = torch.meshgrid(x, y) 
 
-def generateBatch(images, poses, camera_info, args):
+    x = x.T
+    y = y.T
+
+    x = (x - H * 0.5) / focal
+    y = (y - W * 0.5) / focal
+
+    ray_direction = torch.stack([x, -y, -torch.ones_like(x)], -1)
+
+    Rotation = pose[:3, :3]
+    Translation = pose[:3, -1]
+
+    directions = ray_direction[..., None, :]
+    ray_directions = torch.sum(directions * Rotation, -1)
+    ray_origins = torch.broadcast_to(Translation, ray_directions.shape)
+
+    return ray_origins, ray_directions
+
+def SamplePoints(ray_origins, ray_directions, near, far, N_samples):
     """
     Input:
-        images: all images in dataset
-        poses: corresponding camera pose in world frame
-        camera_info: image width, height, camera matrix
-        args: get batch size related information
+        ray_origins: origins of input rays
+        ray_directions: direction of input rays
+        near: near range
+        far: far range
+        N_samples: number of sample per ray
     Outputs:
-        A set of rays
+        sampled points
     """
-    # TODO not sure if batching is to be done?
-    # the notebook does an iteration for just 1 image at a time
-    pass
+    samples = torch.linspace(near, far, N_samples)
+    rays = ray_origins[..., None, :] + t[..., None] * ray_directions[..., None, :]
+    points = rays.reshape(-1, 3)
+    points = positional_encoding(points, num_encoding_functions=4, include_input=True, log_sampling=True)
+
+
+    return points, samples
+
+import os  # Import the "os" module to use its functions
+
+def generateBatch(raypoints, batch_size):
+    """
+    Input:
+        raypoints: sampled points
+        batch_size: number of rays per batch
+    Outputs:
+        batched points
+    """
+    inputs = []  # Define the "inputs" variable as an empty list
+    for i in range(0, len(raypoints), batch_size):
+        inputs.extend(raypoints[i: i + batch_size])  # Append the batched points to the "inputs" list
+
+    return inputs
+     
+
+# To calculate the cumulative product used to calculate alpha
+def cumprod_exclusive(tensor) :
+    dim = -1
+    cumprod = torch.cumprod(tensor, dim)
+    cumprod = torch.roll(cumprod, 1, dim)
+    cumprod[..., 0] = 1.
+    
+    return cumprod
+
+def render(radiance_field, ray_directions, depth_values):
+    """
+    Input:
+        radiance_field: rgb map from model
+        ray_directions: direction of input rays
+        depth_values: number of samples
+    Outputs:
+        rgb_map: rgb color of n depth values
+        depth_map: depth map
+        acc_map: accumulated map
+    """
+
+
+    sigma_a = F.relu(radiance_field[...,3])       #volume density
+    # print("sigma", sigma_a.shape)
+    rgb = torch.sigmoid(radiance_field[...,:3])    #color value at nth depth value
+    # print("rgb", rgb.shape)
+    one_e_10 = torch.tensor([1e10], dtype = ray_directions.dtype, device = ray_directions.device)
+    # print("one_e_10", one_e_10.shape)
+    dists = torch.cat((depth_values[...,1:] - depth_values[...,:-1], one_e_10.expand(depth_values[...,:1].shape)), dim = -1)
+    # print("dists", dists.shape)
+    alpha = 1. - torch.exp(-sigma_a * dists)       
+    weights = alpha * cumprod_exclusive(1. - alpha + 1e-10)     #transmittance
+    rgb_map = (weights[..., None] * rgb).sum(dim = -2)          #resultant rgb color of n depth values
+    depth_map = (weights * depth_values).sum(dim = -1)
+    acc_map = weights.sum(-1)
+
+    return rgb_map, depth_map, acc_map
+
+
     
 
-def render(model, rays_origin, rays_direction, args):
-    """
-    Input:
-        model: NeRF model
-        rays_origin: origins of input rays
-        rays_direction: direction of input rays
-    Outputs:
-        rgb values of input rays
-    """
-    pass
-
 def loss(groundtruth, prediction):
-    pass
+    loss = torch.mean((groundtruth - prediction)**2)
+    return loss
 
 
 def train(images, poses, camera_info, args):
-    pass
+    writer = SummaryWriter(args.logs_path)
+    dataset = loadDataset(args.data_path, 'train')
+
+    model = NeRFmodel()
+    model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lrate)
+
     
 
 def test(images, poses, camera_info, args):
@@ -122,10 +201,10 @@ def configParser():
     parser.add_argument('--data_path',default="./Phase2/Data/leg2o/",help="dataset path")
     parser.add_argument('--mode',default='train',help="train/test/val")
     parser.add_argument('--lrate',default=5e-4,help="training learning rate")
-    parser.add_argument('--n_pos_freq',default=10,help="number of positional encoding frequencies for position")
+    parser.add_argument('--n_pos_freq',default=6,help="number of positional encoding frequencies for position")
     parser.add_argument('--n_dirc_freq',default=4,help="number of positional encoding frequencies for viewing direction")
     parser.add_argument('--n_rays_batch',default=32*32*4,help="number of rays per batch")
-    parser.add_argument('--n_sample',default=400,help="number of sample per ray")
+    parser.add_argument('--n_sample',default=64,help="number of sample per ray")
     parser.add_argument('--max_iters',default=10000,help="number of max iterations for training")
     parser.add_argument('--logs_path',default="./logs/",help="logs path")
     parser.add_argument('--checkpoint_path',default="./Phase2/example_checkpoint/",help="checkpoints path")
