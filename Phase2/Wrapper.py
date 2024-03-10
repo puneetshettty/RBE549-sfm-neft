1ded8e979e5c2d5e184d49b3d881bfd8f02270ae
Edwin Clement <eclement@login-01.cm.cluster>
2024-03-10 21:18:56 +0000

    cur status

--- Phase2/Wrapper.py
+++ Phase2/Wrapper.py
@@ -390,9 +390,6 @@ def train(images, poses, camera_info, args):
         # print("Batch generated")
         # print(f'number of batches: {len(batches)}')
 
-
-
-
         # Forward pass
         prediction = [model(batch) for batch in batches]
         # print(f'shape of prediction: {prediction[0].shape}')
@@ -474,15 +471,15 @@ def main(args):
 
 def configParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path',default=".\data\lego",help="dataset path")
+    parser.add_argument('--data_path',default="./data/lego",help="dataset path")
     parser.add_argument('--mode',default='train',help="train/test/val")
     parser.add_argument('--lrate',default=5e-4,help="training learning rate")
     parser.add_argument('--n_pos_freq',default=6,help="number of positional encoding frequencies for position")
     parser.add_argument('--n_dirc_freq',default=4,help="number of positional encoding frequencies for viewing direction")
-    parser.add_argument('--batchsize',default=1024*4,help="batchsize")
-    parser.add_argument('--n_sample',default=8,help="number of sample per ray")
-    parser.add_argument('--n_rays',default=64,help="number of rays")
-    parser.add_argument('--max_iters',default=10000,help="number of max iterations for training")
+    parser.add_argument('--batchsize',default=1024*4*2,help="batchsize")
+    parser.add_argument('--n_sample',default=64,help="number of sample per ray")
+    parser.add_argument('--n_rays',default=64*2,help="number of rays")
+    parser.add_argument('--max_iters',default=20000,help="number of max iterations for training")
     parser.add_argument('--logs_path',default="./logs/",help="logs path")
     parser.add_argument('--checkpoint_path',default="./checkpoints/",help="checkpoints path")
     parser.add_argument('--load_checkpoint',default=True,help="whether to load checkpoint or not")
